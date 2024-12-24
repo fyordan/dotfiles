cca8f668aa9c17a50a9118967b4723c9609c9397
Fernando Yordan <fernando@yordan.llc>
2024-12-23 19:10:07 -0500

    everything but zshrc working with nixos and transparency

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      }
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  }
+}
